a9c72c2cf6a494b7f966fc08875f9c8694b15703
Sayeed Tahmid <sayeedtahmid07@gmail.com>
2026-09-14 02:53:27 +0600

    Extract game parameters into named constants

--- projects/01-guessing-game/game.py
+++ projects/01-guessing-game/game.py
@@ -1,43 +1,38 @@
-
 import random
 
-keep_playing=True
+keep_playing = True
 
-while keep_playing: 
+while keep_playing:
+    set_num = random.randint(1, 100)
+    attempts_left = 5
+    has_won = False
 
- set_num=random.randint(1,100)
- attempts_left=5
- has_won=False
+    while attempts_left > 0 and not has_won:
+        while True:
+            try:
+                guess_num = int(input("Enter a number from 1-100: "))
+                if 1 <= guess_num <= 100:
+                    break
+                print("Number must be 1 to 100 only")
 
- while attempts_left>0 and not has_won:
+            except ValueError:
+                print("Not a number")
 
-   while True:  
-    
-    try:
-      guess_num = int(input("Enter a number from 1-100: "))
-      if 1<= guess_num <=100:
-        break
-      print ("Number must be 1 to 100 only")
-      
-    except ValueError:
-      print ("Not a number")
-      
-   attempts_left-=1 
+        attempts_left -= 1
 
-   if guess_num == set_num:
-       print ("Correct")
-       has_won = True
-   elif guess_num > set_num:
-       print ("high")
-    
-   else:
-       print ("low")
+        if guess_num == set_num:
+            print("Correct")
+            has_won = True
+        elif guess_num > set_num:
+            print("high")
 
-   print("attempts left = ",attempts_left)    
+        else:
+            print("low")
 
- if not has_won:
-  print ("correct num",set_num)
+        print("attempts left = ", attempts_left)
 
- answer= str(input("Play Again ? y/n: "))
- keep_playing =  answer.lower() == "y"
+    if not has_won:
+        print("correct num", set_num)
 
+    answer = str(input("Play Again ? y/n: "))
+    keep_playing = answer.lower() == "y"
