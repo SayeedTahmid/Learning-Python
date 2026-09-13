8738b435a1f09babcf46191d74e1d2f284861720
Sayeed Tahmid <sayeedtahmid07@gmail.com>
2026-09-14 04:05:45 +0600

    Extract game logic into play_round function

--- projects/01-guessing-game/game.py
+++ projects/01-guessing-game/game.py
@@ -15,30 +15,40 @@ def get_valid_guess(low, high):
         except ValueError:
             print("Not a number")
 
+def play_round(low,high,max_attempts):
+     set_num = random.randint(low, high)
+     attempts_left = max_attempts
+     has_won = False
+
+     while attempts_left > 0 and not has_won:
+     
+                 guess_num = get_valid_guess(low, high)
+     
+                 attempts_left -= 1
+     
+
+                 if guess_num == set_num:
+                     print("Correct")
+                     has_won = True
+                 elif guess_num > set_num:
+                     print("high")
+
+                 else:
+                     print("low")
+
+                 print(f"{attempts_left} left")
+
+     if not has_won:
+         print("correct num", set_num)
+
+     return has_won    
+
+               
 while keep_playing:
-    set_num = random.randint(LOW, HIGH)
-    attempts_left = MAX_ATTEMPTS
-    has_won = False
 
-    while attempts_left > 0 and not has_won:
-
-        guess_num = get_valid_guess(LOW, HIGH)
-
-        attempts_left -= 1
-
-        if guess_num == set_num:
-            print("Correct")
-            has_won = True
-        elif guess_num > set_num:
-            print("high")
-
-        else:
-            print("low")
-
-        print(f"{attempts_left} left")
-
-    if not has_won:
-        print("correct num", set_num)
-
+    play_round(LOW,HIGH,MAX_ATTEMPTS)
     answer = input("Play Again ? y/n: ")
     keep_playing = answer.lower() == "y"
+
+
+   
